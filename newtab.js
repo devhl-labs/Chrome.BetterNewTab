82f9811dcd88f4ec3f4da89823cd5b33ed1aab4e
devhl <shawnkanyer@gmail.com>
2022-11-05 15:44:07 -0400

    replaced string variables with object

--- newtab.js
+++ newtab.js
@@ -23,7 +23,7 @@ window.addEventListener("load", function(){
         var maxLength = topSites.length > 8 ? 8 : topSites.length;
         for (var i = 0; i < maxLength; i++) {
             if (i == 4) { topSitesTable += "</tr><tr>"; }
-            addSpeedDial(topSites[i].title, topSites[i].url, i);
+            addSpeedDial(topSites[i], i);
         }
         topSitesTable += "</tr></table>";
         var items = document.createRange().createContextualFragment(topSitesTable);
@@ -31,20 +31,11 @@ window.addEventListener("load", function(){
     });
 });
 
-function addSpeedDial(topSiteTitle, topSiteUrl, count) {
-    //receives top site data and creates the string to append to the page
-    //the image to be used is stored in the alt property
-    //it will be changed by another function later
-    //PARAMETERS
-    //
-    //topSiteTitle      the topSiteTitle of the website ex."Facebook"
-    //topSiteUrl        the url of the website
-    //count             a simple count of 0 through 7
-    //RESULT            this function will modify the topSitesTable global variable
-    if (topSiteTitle.length > 30) {
-        topSiteTitle = topSiteTitle.substr(0, 27) + "...";
+function addSpeedDial(topSite, count) {
+    if (topSite.title.length > 30) {
+        topSite.title = topSite.title.substr(0, 27) + "...";
     }
-    let domain = (new URL(topSiteUrl));
+    let domain = (new URL(topSite.url));
     domain = domain.hostname
         .replace("www.", "")
         .replace(".com", "")
@@ -53,9 +44,9 @@ function addSpeedDial(topSiteTitle, topSiteUrl, count) {
         .replace(".gov", "")
         .replace("org", "");
 
-    var thumbnail = topSiteUrl + "favicon.ico"
+    var thumbnail = topSite.url + "favicon.ico"
 
-    topSitesTable += '<td><a href="' + topSiteUrl + '" id="a' + count + '" class="speedDialLink plaintext">';
+    topSitesTable += '<td><a href="' + topSite.url + '" id="a' + count + '" class="speedDialLink plaintext">';
     topSitesTable += '<img id="img" src="' + thumbnail + '" alt="Favicon for ' + thumbnail + '" class="speedDialImage"/>';
     topSitesTable += '<div><span>' + domain + '</span></div>'
     topSitesTable += '</a></td>'
