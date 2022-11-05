af87c68d9d1a9bf8c3c19083f55d5e8b92faa601
devhl <shawnkanyer@gmail.com>
2022-11-05 13:38:20 -0400

    convert tabs to spaces

--- newtab.js
+++ newtab.js
@@ -1,88 +1,88 @@
 $(window).bind(
-	"load", function() {
-		renderNew();
-	}
+    "load", function() {
+        renderNew();
+    }
 );
 
 var strTopSites="<table id=\"topSitesTable\"><tr>";
 
 function renderNew(){
-	$("#topSites").click(function(e){
-		showSpeedDial();
-	});
+    $("#topSites").click(function(e){
+        showSpeedDial();
+    });
 
-	//this grabs top site info and passes info to addSpeedDial function
-	chrome.topSites.get(function(topSites){
-		var maxLength = topSites.length > 8 ? 8 : topSites.length;
-		for(var i = 0; i < maxLength; i++){
-			if(i==4){strTopSites+="</tr><tr>";}
-			addSpeedDial(topSites[i].title,topSites[i].url, i);
-		}
-		strTopSites+="</tr></table>";
-		$('#speedDial').append(strTopSites);
-	});
+    //this grabs top site info and passes info to addSpeedDial function
+    chrome.topSites.get(function(topSites){
+        var maxLength = topSites.length > 8 ? 8 : topSites.length;
+        for(var i = 0; i < maxLength; i++){
+            if(i==4){strTopSites+="</tr><tr>";}
+            addSpeedDial(topSites[i].title,topSites[i].url, i);
+        }
+        strTopSites+="</tr></table>";
+        $('#speedDial').append(strTopSites);
+    });
 
-	showSpeedDial();
-	
-	$('#history')
+    showSpeedDial();
+    
+    $('#history')
         .click(function(e) {
             chrome.tabs.update({
                 url: 'chrome://history/'
             });
         });
-		
-	$('#chromeSettings').click(function(e) {
+        
+    $('#chromeSettings').click(function(e) {
         chrome.tabs.update({
             url: 'chrome://settings/'
         });
     });
-		
-	$('#extensions').click(function(e) {
-			chrome.tabs.update({
-				url: 'chrome://extensions/'
-			});
+        
+    $('#extensions').click(function(e) {
+            chrome.tabs.update({
+                url: 'chrome://extensions/'
+            });
         }
-	);
-		
-	$("body").show();
+    );
+        
+    $("body").show();
 }
 
 
 function showSpeedDial(){
-	//show the speed dial, hide everything else
-	localStorage["currentScreen"] = "speedDial";
-	$(".search").hide();
-	$(".speedDial").show();
-	$(".apps").hide();
+    //show the speed dial, hide everything else
+    localStorage["currentScreen"] = "speedDial";
+    $(".search").hide();
+    $(".speedDial").show();
+    $(".apps").hide();
 }
 
 function addSpeedDial(name, link, intCount){
-	//receives top site data and creates the string to append to the page
-	//the image to be used is stored in the alt property
-	//it will be changed by another function later
-	//PARAMETERS
-	//
-	//name		the name of the website ex."Facebook"
-	//link		the url of the website
-	//intCount	a simple count of 0 through 7
-	//			this is needed for changing the image as mentioned above
-	//RESULT	this function will modify the strTopSites global variable
-	if(name.length > 30){
-		name = name.substr(0, 27)+"...";
-	}
-	let domain = (new URL(link));
-	domain = domain.hostname
-		.replace("www.", "")
-		.replace(".com", "")
-		.replace(".edu", "")
-		.replace(".co.uk", "")
-		.replace(".gov", "")
-		.replace("org", "");
+    //receives top site data and creates the string to append to the page
+    //the image to be used is stored in the alt property
+    //it will be changed by another function later
+    //PARAMETERS
+    //
+    //name		the name of the website ex."Facebook"
+    //link		the url of the website
+    //intCount	a simple count of 0 through 7
+    //			this is needed for changing the image as mentioned above
+    //RESULT	this function will modify the strTopSites global variable
+    if(name.length > 30){
+        name = name.substr(0, 27)+"...";
+    }
+    let domain = (new URL(link));
+    domain = domain.hostname
+        .replace("www.", "")
+        .replace(".com", "")
+        .replace(".edu", "")
+        .replace(".co.uk", "")
+        .replace(".gov", "")
+        .replace("org", "");
 
-	var thumbnail = link + "favicon.ico"
+    var thumbnail = link + "favicon.ico"
 
-	strTopSites+='<td><a href="' + link + '" id="a' + intCount + '" class="speedDialLink plaintext">';
-	strTopSites+='<img id="img" src="' + thumbnail + '" alt="Favicon for ' + thumbnail + '" class="speedDialImage"/>';
-	strTopSites+='<div><span>' + domain + '</span></div>'
-	strTopSites+='</a></td>'
+    strTopSites+='<td><a href="' + link + '" id="a' + intCount + '" class="speedDialLink plaintext">';
+    strTopSites+='<img id="img" src="' + thumbnail + '" alt="Favicon for ' + thumbnail + '" class="speedDialImage"/>';
+    strTopSites+='<div><span>' + domain + '</span></div>'
+    strTopSites+='</a></td>'
 }
