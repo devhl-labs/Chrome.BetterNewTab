c1cb67beb132f419fab0c12821754207640f9e60
devhl <shawnkanyer@gmail.com>
2022-11-05 15:55:21 -0400

    use string interpolation

--- newtab.js
+++ newtab.js
@@ -42,8 +42,13 @@ function addSpeedDial(topSite) {
 
     var faviconUrl = topSite.url + "favicon.ico"
 
-    topSitesTable += '<td><a href="' + topSite.url + '" class="speedDialLink plaintext">';
-    topSitesTable += '<img src="' + faviconUrl + '" alt="Favicon for ' + domain + '" class="speedDialImage"/>';
-    topSitesTable += '<div><span>' + domain + '</span></div>'
-    topSitesTable += '</a></td>'
+    topSitesTable += `
+<td>
+    <a href="${topSite.url}" class="speedDialLink plaintext">
+        <img src="${faviconUrl}" alt="Favicon for ${domain}" class="speedDialImage"/>
+        <div>
+            <span>${domain}</span>
+        </div>
+    </a>
+</td>`
 }
